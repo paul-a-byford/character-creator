8108a305ef7fd5fb41804bfbe8aee07ec24aa10e
paul-byford <paul@ablely.co>
2021-03-18 20:49:37 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
+import PerspectiveBar from './components/PerspectiveBar'
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+      <PerspectiveBar />
       <CharacterEditor />
       <Footer />
     </>
